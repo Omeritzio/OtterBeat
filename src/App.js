fe3b19910cad8b2ae0a278052eefd5ef1ec9c4be
Omeritzio <omershlomo441@gmail.com>
2023-05-21 02:28:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App ()  {
+  const [songs, setSongs] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = (songId) => {
+    if (favorites.includes(songId)) {
+      // If the song is already in favorites, remove it
+      setFavorites(favorites.filter((id) => id !== songId));
+    } else {
+      // If the song is not in favorites, add it
+      setFavorites([...favorites, songId]);
+    }
+  };
+
+  useState(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/Songs.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch JSON data');
+        }
+        const data = await response.json();
+        setSongs(data);
+      } catch (error) {
+        console.error('Error fetching JSON:', error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Songs</h1>
+      {songs.map((artist, index) => (
+        <div key={index}>
+          <h2>{artist.artist}</h2>
+          <ul>
+            {artist.songs.map((song, songIndex) => (
+              <li key={songIndex}>
+                <h3>{song.title}</h3>
+                <button onClick={() => toggleFavorite(song.id)}>
+                  {favorites.includes(song.id) ? (
+                    <span role="img" aria-label="Red Heart">
+                      ❤️
+                    </span>
+                  ) : (
+                    <span role="img" aria-label="White Heart">
+                      🤍
+                    </span>
+                  )}
+                </button>
+                <p>Duration: {song.duration}</p>
+                <p>Release Year: {song.releaseYear}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
