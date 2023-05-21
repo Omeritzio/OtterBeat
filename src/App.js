1b8e10037fa93159f919af406f55608bd0fdd93c
Omeritzio <omershlomo441@gmail.com>
2023-05-21 11:10:43 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Songs from './components/songs';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
 
 function App ()  {
   
-
-  
-
-
-
   return (
     <div>
-      <h1>Songs</h1>
-      <Songs/>
+      <Router>
+      <Routes>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route path="/" element={<Home/>} />
+        <Route path="/songs" element={<Songs/>} />
+        {/* <Route path="/contact" component={Contact} /> */}
+       
+    </Routes>
+    </Router>
+      
     </div>
   );
 };
---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+
+const Home=() => {
+    return(
+        <div>
+<blockquote className="imgur-embed-pub" lang="en" data-id="a/rMQtB1Z"  ><a href="//imgur.com/a/rMQtB1Z"></a></blockquote>
+      
+            <Link to="/songs">
+            <button> 
+                Songs
+            </button>
+            </Link>
+        </div>
+    )
+}
+
+export default Home
