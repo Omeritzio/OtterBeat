deeb562da9546db634460225d793bcadf1dd1028
Omeritzio <omershlomo441@gmail.com>
2023-05-21 03:03:04 +0300

    first commit

---
+++ src/Song.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Song =() => {
+    return (
+        <div className="bg-light-green dib br3 pa3 grow bw2 shadow-5">
+
+        </div>
+    )
+}
+
+export default Song
